6eeb0304439372bc35dbfdc0e43bceba7d52ebb9
David-CKJ <davidckjgeneral@gmail.com>
2026-01-07 07:15:30 +0800

    Fix keycloak login bug

--- frontend/src/services/keycloak.js
+++ frontend/src/services/keycloak.js
@@ -1,9 +1,15 @@
+// src/services/keycloak.js
 import Keycloak from "keycloak-js";
 
+const url = "https://keycloak.ltu-m7011e-9.se";
+if (!url) {
+  throw new Error("Missing VITE_KEYCLOAK_URL (must be set at build time)");
+}
+
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL || "http://localhost:8080",
-  realm: import.meta.env.VITE_KEYCLOAK_REALM || "mintify",
-  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || "mintify-frontend",
+  url,
+  realm: mintify,
+  clientId: mintify-frontend,
 });
 
 export default keycloak;
